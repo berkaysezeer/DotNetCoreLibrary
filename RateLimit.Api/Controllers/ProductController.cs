94f28f7af33dd4c23176798782c2eee535bb9d9b
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-01 15:31:34 +0300

    Parametreli Endpoint

--- RateLimit.Api/Controllers/ProductController.cs
+++ RateLimit.Api/Controllers/ProductController.cs
@@ -13,6 +13,12 @@ namespace RateLimit.Api.Controllers
             return Ok(new { Id = 1, Name = "Kalem", Price = 20 });
         }
 
+        [HttpGet("{name}")]
+        public IActionResult GetProduct(string name)
+        {
+            return Ok(name);
+        }
+
         [HttpPut]
         public IActionResult UpdateProduct()
         {
