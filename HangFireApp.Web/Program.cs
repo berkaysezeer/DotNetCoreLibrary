a1cbac494e5d67a77444e56519b681da0d71d7c9
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-04 00:44:54 +0300

    Configuration

--- HangFireApp.Web/Program.cs
+++ HangFireApp.Web/Program.cs
@@ -1,7 +1,18 @@
+using Hangfire;
+using HangFireApp.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//sql server kullanacaðý yolu belirteceðiz
+var connectionString = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+
+//projenin ayný zamanda bir hangfire server olmasýný saðlýyoruz
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -23,5 +34,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//dashboard aktif olmasý için ekliyoruz
+app.UseHangfireDashboard();
 
 app.Run();
