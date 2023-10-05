8277c32b632809c5e2abcf9aabff8bb4fc2ba142
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-05 21:26:37 +0300

    Developer Exception Page

---
+++ ErrorHandlingApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ErrorHandlingApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ErrorHandlingApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int value1 = 5, value2 = 0;
+            int result = value1 / value2;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
