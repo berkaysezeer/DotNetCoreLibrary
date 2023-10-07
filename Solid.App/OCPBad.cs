7353d73f3085d6a0c8525c24757c9cf294ec2cbf
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-07 22:50:12 +0300

    OCP için kötü kod örneği

---
+++ Solid.App/OCPBad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.OCPBad
+{
+
+    /*
+     Eğer ileride yeni bir SalaryType eklenirse switch kısmına yeni case eklememiz gerekiyor
+    Open/close prensibine aykırı oluyor
+     */
+    public class SalaryCalculator
+    {
+        public decimal Calculate(decimal salary, SalaryType salaryType)
+        {
+            decimal newSalary = 0;
+
+            switch (salaryType)
+            {
+                case SalaryType.Low:
+                    newSalary = salary * 2;
+                    break;
+                case SalaryType.High:
+                    newSalary = salary * 6;
+                    break;
+                case SalaryType.Medium:
+                    newSalary = salary * 4;
+                    break;
+                default:
+                    break;
+            }
+
+            return newSalary;
+        }
+    }
+
+    public enum SalaryType
+    {
+        Low, Medium, High
+    }
+}
--- Solid.App/Program.cs
+++ Solid.App/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Solid.App.OCPBad;
+
 Console.WriteLine("Hello, World!");
+
+
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+Console.WriteLine($"Low: {salaryCalculator.Calculate(1000, SalaryType.Low)}");
+Console.WriteLine($"Medium: {salaryCalculator.Calculate(1000, SalaryType.Medium)}");
+Console.WriteLine($"High: {salaryCalculator.Calculate(1000, SalaryType.High)}");
