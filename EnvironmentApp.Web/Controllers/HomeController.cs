547838fc1cdd03030af5d64d0e18de4e439dc4d2
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-07 21:23:59 +0300

    Environment

---
+++ EnvironmentApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EnvironmentApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EnvironmentApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            throw new Exception("Beklenmedik bir hata ile karşılaşıldı");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
