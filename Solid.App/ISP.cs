21144e381abd70680ed58e07a7f68c2b581643cd
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-08 08:53:15 +0300

    ISP için iyi kod örneği

--- Solid.App/ISP.cs
+++ Solid.App/ISP.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Solid.App.ISP
 {
-    #region Bad
     //class library Read Impl.
     //class library Create/Update/Delete Impl
 
@@ -17,7 +16,52 @@ namespace Solid.App.ISP
     }
 
     //Classın sadece okuma görevi varken Create/Update/Delete işlemlerini de yüklemiş olduk
-    public class ReadProductRepository : IProductRepository
+    #region Kötü örnek
+    //public class ReadProductRepository : IProductRepository
+    //{
+    //    public Product Create(Product p)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Product Delete(Product p)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public List<Product> GetAll()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Product GetById(int id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Product Update(Product p)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+    #endregion
+
+    //interfaceler ve classlar genel olarak kullanılması uygun değildir görevine ve işlevine göre gerektiği kadar parçalanmalıdır
+    #region İyi kod
+    public class ReadProductRepository : IReadRepository
+    {
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WriteProductRepository : IWriteRepository
     {
         public Product Create(Product p)
         {
@@ -29,28 +73,35 @@ namespace Solid.App.ISP
             throw new NotImplementedException();
         }
 
-        public List<Product> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
         public Product Update(Product p)
         {
             throw new NotImplementedException();
         }
     }
 
+    //kötü interface örneği
     public interface IProductRepository
     {
         //Read Impl
         List<Product> GetAll();
         Product GetById(int id);
 
+        //Create/Update/Delete
+        Product Create(Product p);
+        Product Update(Product p);
+        Product Delete(Product p);
+    }
+
+
+    public interface IReadRepository
+    {
+        //Read Impl
+        List<Product> GetAll();
+        Product GetById(int id);
+    }
+
+    public interface IWriteRepository
+    {
         //Create/Update/Delete
         Product Create(Product p);
         Product Update(Product p);
